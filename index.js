5375b14fc9bb59b9f4d2b45a1a13776e495079d8
Sergey Mazur <mazzurs@ya.ru>
2018-01-25 11:07:19 +0200

    Everything is working

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ box.onclick = function (event) {
         var newDiv = document.createElement("div");         // создаем новый элемент
         fillWithContent(newDiv);                            // наполняем элемент контентом
         newDiv.setAttribute('class', 'tag');                // присваиваем класс tag
-        newDiv.addEventListener('dblclick', stateToggle);   // при двойном клике меняем состояние тега
+        newDiv.addEventListener('dblclick', changeState);   // при двойном клике меняем состояние тега
+        newDiv.addEventListener('touchend', changeState);   // при двойном клике меняем состояние тега
         box.appendChild(newDiv);                            // вставляем элемент в коробку
         setCoordinates(newDiv);                             // задаем координаты
         input.value = '';                                   // очищаем инпут
-        newDiv.touchAction
     }
 };
 
@@ -19,7 +19,7 @@ function setCoordinates(div) {                                     // задае
     var x = event.x - coordinates.left - coordinates.width / 2;
     var y = event.y - coordinates.top - 10;
 
-    if (y < 0) {
+    if (y < 0) {          // не допускаем установку тега за пределами бокса
         y = -1;
     } else {
         if (y > 483) {
@@ -31,12 +31,11 @@ function setCoordinates(div) {                                     // задае
     } else {
         if (x + coordinates.width > 512) {
             x = 512 - coordinates.width;
-            // div.classList.toggle('reverse')
         }
     }
 
-    if (x + coordinates.width > 512 - 23.56) {
-        div.classList.toggle('reverse')
+    if (x + coordinates.width > 512 - 23.56) { // если слишком близко к правому краю бокса, то
+        div.classList.toggle('reverse')        // переносим remover влево
     }
 
     div.style.top = y + 'px';         // задаем координаты по вертикали
@@ -51,10 +50,12 @@ function fillWithContent(div) {
     remover.classList.toggle('hide');              // по умолчанию прячем remover
     remover.innerText = "X";
     remover.addEventListener('click', remove);
+    remover.addEventListener('touchstart', remove);
 
     text.innerText = input.value;
     text.setAttribute('class', 'text');
     text.addEventListener('mousedown', draggAndDrop);
+    text.addEventListener('touchmove', draggAndDrop);
 
     div.setAttribute('draggable', 'false');
     div.setAttribute('isOpen', 'false');
@@ -67,7 +68,7 @@ function remove(event) {
     event.currentTarget.parentNode.remove();
 }
 
-function stateToggle(event) {
+function changeState(event) {
     var tag = event.currentTarget;
     var text = tag.children[0];
     var remover = tag.children[1];
@@ -100,44 +101,45 @@ function draggAndDrop(e) {
     var boxWidth = boxCoordinates.width;
     var boxHeight = boxCoordinates.height;
 
-    var tagX = tagCoordinates.left;
-    var tagY = tagCoordinates.top;
     var tagWidth = tagCoordinates.width;
     var tagHeight = tagCoordinates.height;
 
     var x;
     var y;
+    var touchobj;
 
+    if (tag.attributes[0].value == 'true') {  // если состояние тега позволяет драгать, то
 
-    if (tag.attributes[0].value == 'true') {
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) { // для мобильного отдельная реализация drag'n drop
+            tag.ontouchmove = function () {
+                touchobj = e.changedTouches[0];      // первая точка прикосновения
+                touchX = parseInt(touchobj.clientX); // положение точки касания по x
+                touchY = parseInt(touchobj.clientY); // положение точки касания по y
 
-        body.onmousemove = function (event) {
-            if (tag.attributes[0].value == 'true') {
                 tagCoordinates = tag.children[0].getBoundingClientRect();
-                tagX = tagCoordinates.left;
-                tagY = tagCoordinates.top;
 
-                x = event.pageX - boxLeft - tagWidth / 2;
-                y = event.pageY - boxTop - 15;
 
-                if (event.pageX - tagWidth / 2 < boxLeft) {                 // если за пределами левой границы
+                x = touchX - boxLeft - tagWidth / 2;
+                y = touchY - boxTop - 15;
+
+                if (touchX - tagWidth / 2 < boxLeft) {                 // если за пределами левой границы
                     x = 0;                                                  // то отступ слева = 0
                 } else {                                                    // иначе проверяем
-                    if (event.pageX + tagWidth / 2 > boxRight) {            // если за пределами правой границы
+                    if (touchX + tagWidth / 2 > boxRight) {            // если за пределами правой границы
                         x = boxWidth - tagWidth;                            // то отступ слева = ширина бокса - ширина тега
                     }
                 }
-                if (event.pageY > boxBottom - tagHeight / 2) {              // если за пределами нижней границы
+                if (touchY > boxBottom - tagHeight / 2) {              // если за пределами нижней границы
                     y = boxHeight - tagHeight;                              // то отступ сверху = высота бокса - высота тега
                 } else {                                                    // иначе проверяем
-                    if (event.pageY < boxTop + tagHeight / 2) {             // если за пределами верхней границы
+                    if (touchY < boxTop + tagHeight / 2) {             // если за пределами верхней границы
                         y = 0;                                              // то отступ сверху = 0
                     }
                 }
 
-                if (event.pageX + 23.56 + tagCoordinates.width / 2 > boxRight) {    // если слишком близко к правому краю
+                if (touchX + tag.children[1].offsetWidth + tagCoordinates.width / 2 > boxRight) {         // если слишком близко к правому краю
                     tag.attributes[2].value = 'tag reverse';                        // то меняем елементы местами
-                    x -= 23.56;                                                     // и от отступа слева отнимаем ширину ремувера
+                    x -= tag.children[1].offsetWidth;                               // и от отступа слева отнимаем ширину ремувера
                 } else {                                                            // иначе
                     tag.attributes[2].value = 'tag';                                // восстанавливаем обычный порядок элементов
                 }
@@ -145,12 +147,53 @@ function draggAndDrop(e) {
                 tag.style.left = x + 'px';      // наконец-то присваиваем значения отступам
                 tag.style.top = y + 'px';
 
-            }
+                tag.ontouchend = function () {  // и прекращаем драг
+                    tag.ontouchmove = null;
+                }
+            };
+        } else {
+            body.onmousemove = function (event) {
+                if (tag.attributes[0].value == 'true') {
+                    tagCoordinates = tag.children[0].getBoundingClientRect();
 
-            body.onmouseup = function () {      // и прекращаем драг когда отпускаем ЛКМ
-                body.onmousemove = null;
+
+                    x = event.pageX - boxLeft - tagWidth / 2;
+                    y = event.pageY - boxTop - 15;
+
+                    if (event.pageX - tagWidth / 2 < boxLeft) {                 // если за пределами левой границы
+                        x = 0;                                                  // то отступ слева = 0
+                    } else {                                                    // иначе проверяем
+                        if (event.pageX + tagWidth / 2 > boxRight) {            // если за пределами правой границы
+                            x = boxWidth - tagWidth;                            // то отступ слева = ширина бокса - ширина тега
+                        }
+                    }
+                    if (event.pageY > boxBottom - tagHeight / 2) {              // если за пределами нижней границы
+                        y = boxHeight - tagHeight;                              // то отступ сверху = высота бокса - высота тега
+                    } else {                                                    // иначе проверяем
+                        if (event.pageY < boxTop + tagHeight / 2) {             // если за пределами верхней границы
+                            y = 0;                                              // то отступ сверху = 0
+                        }
+                    }
+
+                    if (event.pageX + tag.children[1].offsetWidth + tagCoordinates.width / 2 > boxRight) {    // если слишком близко к правому краю
+                        tag.attributes[2].value = 'tag reverse';                        // то меняем елементы местами
+                        x -= tag.children[1].offsetWidth;                                                     // и от отступа слева отнимаем ширину ремувера
+                    } else {                                                            // иначе
+                        tag.attributes[2].value = 'tag';                                // восстанавливаем обычный порядок элементов
+                    }
+
+                    tag.style.left = x + 'px';      // наконец-то присваиваем значения отступам
+                    tag.style.top = y + 'px';
+
+                }
+
+                body.onmouseup = function () {      // и прекращаем драг когда отпускаем ЛКМ
+                    body.onmousemove = null;
+                }
             }
         }
+
+
     }
 }
 
